08d33f4e6ebad621c86f5773c18a12a91f1505ed
Elaine Bastos <elaineb80@hotmail.com>
2024-01-26 21:02:03 -0300

    Finalizado o arquivo script

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
-//BOM DIA | BOA TARDE | BOA NOITE
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
 
-// Obtém a referência do elemento com o ID "greeting"
-const greetingElement = document.getElementById("greeting");
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
 
-// Obtém a hora atual do sistema
-const currentHour = new Date().getHours();
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
 
-// Define a saudação com base na hora atual
-// if (currentHour >= 5 && currentHour < 12) {
-//   greetingElement.textContent = "Bom dia";
-// } else if (currentHour >= 12 && currentHour < 18) {
-//   greetingElement.textContent = "Boa tarde";
-// } else {
-//   greetingElement.textContent = "Boa noite";
-// }
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
 
-// Forma mais simples
-const greetingMessage =
-  currentHour >= 5 && currentHour < 12
-    ? "Bom dia"
-    : currentHour >= 12 && currentHour < 18
-    ? "Boa tarde"
-    : "Boa noite";
+    resultArtist.classList.remove('hidden');
+}
 
-greetingElement.textContent = greetingMessage;
-
-// GRID INTELIGENTE
-const container = document.querySelector(".offer__list-item");
-
-const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
-  const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
-  const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
-
-  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
-  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
-
-  console.log({ container });
-  console.log({ numColumns });
-});
-//observando a mudança do elemento
-observer.observe(container);
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
